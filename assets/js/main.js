30105e03b07167aedc6ab2ceff9c9e9c3ef2f3f1
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-12-19 16:33:35 -0500

    added about section in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const showMenu = (toggleId, navID) => {
     const nav = document.getElementById(navID)
 
     if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
@@ -13,8 +13,14 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
+    // removes any 'active' class name on any of the navLinks
     navLink.forEach(n => n.classList.remove('active'))
+    // adds a 'active' class name of the CLICKED navlink
     this.classList.add('active')
+
+    // on menu item selection, navbar then disapears to the right again
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
